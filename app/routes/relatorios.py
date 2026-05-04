c64f9615b86fc6880ee0e124d28e90ad2b1fdb76
Aline Alves <alinealves_99@hotmail.com>
2026-05-03 23:43:46 -0300

    feat: listagem de eventos indeferidos com controle de acesso

--- app/routes/relatorios.py
+++ app/routes/relatorios.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, jsonify
 from app.services.relatorio_service import gerar_relatorio
-from app.utils.auth_utils import login_required, roles_required
+from app.utils.auth_utils import login_required, role_required
 
 relatorios_bp = Blueprint('relatorios', __name__)
 
 @relatorios_bp.route('/relatorios', methods=['GET'])
 @login_required
-@roles_required('ADMINISTRADOR', 'SECRETARIO')
+@role_required('ADMINISTRADOR', 'SECRETARIO')
 def relatorio_dashboard():
     dados = gerar_relatorio()
     return jsonify(dados), 200
